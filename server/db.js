c8caaf7a1f44315ed04edc010cbb0cfea68b9a52
YanaT1 <108289396+YanaT1@users.noreply.github.com>
2025-12-31 19:47:24 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 const {Sequelize} = require('sequelize');
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.POSTGRES_DB,
+    process.env.POSTGRES_USER,
+    process.env.POSTGRES_PASSWORD,
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
