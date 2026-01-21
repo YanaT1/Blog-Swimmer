3253619a8bc5a6ed6fb8a5a7c156f34af79bec5f
YanaT1 <108289396+YanaT1@users.noreply.github.com>
2026-01-21 22:34:07 +0100

    Update AuthService.ts

--- client/src/services/AuthService.ts
+++ client/src/services/AuthService.ts
@@ -19,7 +19,7 @@ class AuthService {
         await $authHost.post('/user/logout');
     }
 
-    static async checkAuth()
+    static async refresh()
     : Promise<AxiosResponse<AuthResponse>> {
         return $host.get<AuthResponse>('/user/refresh');
     }
