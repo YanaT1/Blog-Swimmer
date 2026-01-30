fcd5fd4a29d09e58c7ae86a68fd5a7f33a92e2b1
YanaT1 <108289396+YanaT1@users.noreply.github.com>
2026-01-30 20:48:45 +0100

    Update YearsMedalsPage.tsx

--- client/src/pages/YearsMedalsPage.tsx
+++ client/src/pages/YearsMedalsPage.tsx
@@ -23,12 +23,13 @@ function YearsMedalsPage() {
     if (typeOfMedals.isLoading) {
         return <Loader />;
     }
-    if (!filteredMedals.length) 
+    if (!filteredMedals.length) {
         return ( 
             <h2 className='text-center mt-5' style={{ color:'rgba(3, 51, 109, 0.6)'}}>
             No medals found for year {year}.
             </h2>;
         );
+    }
 
     return (
         <>
