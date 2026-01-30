51c140e43741bd4fed034d96734c9f12cc6f74b8
YanaT1 <108289396+YanaT1@users.noreply.github.com>
2026-01-30 22:05:28 +0100

    Update MedalsPage.tsx

--- client/src/pages/MedalsPage.tsx
+++ client/src/pages/MedalsPage.tsx
@@ -1,7 +1,6 @@
 import {
   useContext,
-  useEffect,
-  useMemo} from 'react';
+  useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {observer} from 'mobx-react-lite';
 import {Context} from '../store/store';
